30794ee59f0f5b55c6fc6e6fa8da65989d73941c
Marcelearn <jaworskimarcel@gmail.com>
2022-11-15 20:53:46 +0100

    newComponent Tasks

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { BoredContext } from '../context/Bored';
+import Tasks from '../components/Tasks'
 function Home() {
   const { task } = useContext(BoredContext);
   
@@ -8,10 +9,10 @@ function Home() {
       <h2>We have a {task.type} activity for you!</h2>
       <h1>{task.activity}</h1>
       <ul>
-        
         <li>Participants: {task.participants}</li>
         <li>price: {task.price}</li>
       </ul>
+      <Tasks />
       
     </main>
   )
